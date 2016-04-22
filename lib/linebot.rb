18f54ad2666a6471dd2d27d082a7b31c6fe1b264
tomcha <tomcha@tomcha.net>
2016-04-22 12:47:25 +0900

    コントローラー受け試作

--- lib/linebot.rb
+++ lib/linebot.rb
@@ -5,5 +5,21 @@ require 'yaml'
 
 module Linebot
   class Application < Sinatra::Base
+    post '/' do
+    end
+
+    post '/linebot/callback' do
+      params = JSON.parse(request.body.read)
+
+      params['result'].each do |msg|
+        p msg
+        request_contet = {
+          to: [msg['content']['from']],
+          toChannel: 1383378250,
+          eventType: 140177271400161403,
+            content: msg['content'],
+        }
+      end
+    end
   end
 end
